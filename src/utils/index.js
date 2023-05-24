38e86dcde93e49341b3e442d7f3a0aef6c194f51
Jackson Soriano <jackson.masters.it@gmail.com>
2023-05-25 00:13:58 +0800

    feat: project list added

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 export const cleanObject = (obj) => {
   const filteredEntries = Object.entries(obj).filter(
-    ([value]) => value !== undefined && value !== ""
+    ([key, value]) => value !== undefined && value !== ""
   );
   return Object.fromEntries(filteredEntries);
 };
